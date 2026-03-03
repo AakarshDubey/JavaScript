57dcba28cedc269f8395df7c30364687de3b3ea6
Aakarsh Dubey <aakarshdubey2207@gmail.com>
2026-03-03 14:12:46 +0000

    functions with objects and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -51,4 +51,56 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("aakarsh"))
-console.log(loginUserMessage()) // if we do not pass any value it gives undefined
+// console.log(loginUserMessage()) // if we do not pass any value it gives undefined
+
+
+// Rest Operator (...)
+
+/* Here the rest operator is used in functions 
+where we dont know the amount of values that will
+be passed inside the function.
+
+The Rest Operator binds all the passed values 
+and convert them into an array of values through
+which we can access and use them easily.
+*/
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+} 
+
+//console.log(calculateCartPrice(200, 400, 500));
+
+
+// HOW  TO PASS OBJECT AS A PARAMETER IN FUNCTION
+const user = {
+    username: "hitesh",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+//handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+}) // can pass the object directly without making one
+
+
+
+
+// PASSING ARRAYS IN FUNCTION
+
+const myNewArray = [200, 400, 500, 700]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+//console.log(returnSecondValue(myNewArray)); // passing through array variable
+console.log(returnSecondValue([200, 400, 500, 700])); // passing value directly
+
+
